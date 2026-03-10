b430b4142d0f5d61209f01a05e393873b19280ac
Lenovo <AlexanderHuizinga04@gmail.com>
2026-03-10 15:41:17 -0400

    Tokenizer fully integrated

--- backend/src/main/java/Tokenizer/src/JavaTokenizer.java
+++ backend/src/main/java/Tokenizer/src/JavaTokenizer.java
@@ -5,15 +5,22 @@
 package Tokenizer.src;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  *
- * @author jakes
- */
+
+ @author jakes*/
 public class JavaTokenizer {
+    public JavaTokenizer() throws IOException{
+        MultiLangTokenizer multi = new MultiLangTokenizer();
+        List<Node> l =multi.tokenize("backend/testFiles/Test.java");
+        for(Node n : l){
+            System.out.println("Line Number: " + n.LineNumber + " Token: " + n.token.getText());
+        }
+    }
 
     public static void main(String[] args) throws IOException {
-        MultiLangTokenizer tokenizer = new MultiLangTokenizer();
-        tokenizer.tokenize("backend/testFiles/Test.java");
+        JavaTokenizer j = new JavaTokenizer();
     }
 }
