1d96d6bf5bdbdaea4e22828d8c3a9998f024b4fa
Alireza Firouzinia <alirezafiruzinia21@gmail.com>
2026-03-12 16:05:48 -0400

    Update MultiLangTokenizer.java

--- backend/src/main/java/Tokenizer/src/MultiLangTokenizer.java
+++ backend/src/main/java/Tokenizer/src/MultiLangTokenizer.java
@@ -1,3 +1,4 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -15,9 +16,9 @@ import java.util.List;
 
 public class MultiLangTokenizer {
     List<String> ignoredTokens = new LinkedList();
-    
+
     public MultiLangTokenizer() throws FileNotFoundException, IOException{
-         try (BufferedReader reader = new BufferedReader(new FileReader("backend/src/Main/resources/Ignore.txt"))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/Ignore.txt"))) {
             String line = reader.readLine();
             while(line !=null){
                 ignoredTokens.add(line);
@@ -25,7 +26,7 @@ public class MultiLangTokenizer {
                 line = reader.readLine();
             }
         }
-         
+
     }
 
     public List<Node> tokenize(String filePath) throws IOException {
@@ -35,13 +36,13 @@ public class MultiLangTokenizer {
 
         if (filePath.endsWith(".java")) {
             lexer = new JavaLexer(input);
-        } 
+        }
         else if (filePath.endsWith(".c")) {
             lexer = new CLexer(input);
-        } 
+        }
         else if (filePath.endsWith(".cpp")) {
             lexer = new CPP14Lexer(input);
-        } 
+        }
         else {
             throw new IllegalArgumentException("Unsupported file type");
         }
@@ -49,26 +50,26 @@ public class MultiLangTokenizer {
         Token token;
         List<Node> tokenList = new LinkedList();
         while ((token = lexer.nextToken()).getType() != Token.EOF) {
-    if (token.getChannel() == Token.DEFAULT_CHANNEL) {
+            if (token.getChannel() == Token.DEFAULT_CHANNEL) {
 
-        String tokenName = lexer.getVocabulary().getSymbolicName(token.getType());
+                String tokenName = lexer.getVocabulary().getSymbolicName(token.getType());
 
-        int line = token.getLine();
-        int column = token.getCharPositionInLine();
-        int ignore =0;
-        for(String s : ignoredTokens){
-            if (tokenName.equals(s)){ ignore =1;break;}
-        }
-        if(ignore ==0){
-            Node n = new Node(line,token,tokenName);
-            tokenList.add(n);
-        } 
-        
-    }
+                int line = token.getLine();
+                int column = token.getCharPositionInLine();
+                int ignore =0;
+                for(String s : ignoredTokens){
+                    if (tokenName.equals(s)){ ignore =1;break;}
+                }
+                if(ignore ==0){
+                    Node n = new Node(line,token,tokenName);
+                    tokenList.add(n);
+                }
+
+            }
         }
         return tokenList;
     }
 
 
-    
+
 }
