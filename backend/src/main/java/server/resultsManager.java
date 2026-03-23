ba392ef28186e08fb90d55fec5e77f3b6c41df95
DA-mographic <adventurequestnerd2345@gmail.com>
2026-03-23 10:23:42 -0400

    Results now loop through assignment folder instead of test folder

--- backend/src/main/java/server/resultsManager.java
+++ backend/src/main/java/server/resultsManager.java
@@ -25,27 +25,35 @@ public class resultsManager {
     public resultsManager(SupabaseStorageService storageService, DBHandler dbHandler) {
         this.storageService = storageService;
         this.dbHandler = dbHandler;
-        BUCKET = "TestFiles";
+        BUCKET = "Submissions";
     }
     public void generateResults(MultipartFile file, String course, long assignment, long submissionID) throws IOException {
         log.info("Generating Results:");
-        List<File> files = loadFiles(course, assignment, BUCKET);
+        String prefix = course + "/" + assignment;
+        List<File> files = loadFiles(course, assignment, BUCKET, prefix);
+        List<Long> scores = new LinkedList<Long>();
         Path p1 = zipProcessor.concatZipFromMultipartToTemp(file, "baseFile");;
         File f1 = p1.toFile();
         log.info("Running comparison on " + files.size() + " files.");
+        int topK = dbHandler.getTopK(assignment);
         int i = 1;
 
         for (File f : files) {
             try {
                 log.info("Starting checker on file index={} name={}", i, f.getName());
                 PlagiarismChecker pc = new PlagiarismChecker(f1, f);
-                dbHandler.insertResult(submissionID, i, pc.index, OffsetDateTime.now(), dbHandler.generateResultID());
+                scores.add(pc.index);
+
                 log.info("Checker completed on file index={}", i);
             } catch (Exception e) {
                 log.error("Checker failed on file index={} name={}", i, f.getName(), e);
                 throw e; // keep failing fast so submit returns error
             }
             i++;
+        }
+        int[] toSubmit = getTopScores(topK, scores);
+        for (int j = 0; j < topK; j++) {
+            dbHandler.insertResult(submissionID, j+1, scores.get(toSubmit[j]), OffsetDateTime.now(), dbHandler.generateResultID());
         }
         log.info("All comparisons complete");
         Path temp = Path.of("temp");
@@ -62,16 +70,15 @@ public class resultsManager {
         log.info("Temp files deleted");
     }
 
-    private List<File> loadFiles(String course, long assignment, String bucket) throws IOException {
+    private List<File> loadFiles(String course, long assignment, String bucket, String prefix) throws IOException {
         List<File> files = new LinkedList<>();
-        //String prefix = course + "/" + assignment; // matches your upload path format
 
-        List<String> allPaths = storageService.listAllFiles(null, bucket);
+        List<String> allPaths = storageService.listAllFiles(prefix, bucket);
         int i = 1;
 
         for (String objectPath : allPaths) {
             // adjust this filter if your uploaded extension changes
-            if (!objectPath.toLowerCase().endsWith(".zip")) continue;
+            if (!objectPath.toLowerCase().endsWith(".bin")) continue;
 
             byte[] content = storageService.downloadFile(objectPath, bucket);
 
@@ -88,5 +95,26 @@ public class resultsManager {
         }
 
         return files;
+    }
+
+    private int[] getTopScores(int size, List<Long> scores) {
+        int[] toReturn = new int[size];
+        Long[] temp = scores.toArray(new Long[0]);
+        for (int i = 0; i < size; i++) {
+            int largest = findLargestIndex(temp);
+            toReturn[i] = largest;
+            temp[largest] = 0L;
+        }
+        return toReturn;
+    }
+
+    private int findLargestIndex(Long[] scores) {
+        int toReturn = 0;
+        for (int i = 1; i < scores.length; i++) {
+            if (scores[i] > scores[toReturn]) {
+                toReturn = i;
+            }
+        }
+        return toReturn;
     }
 }
