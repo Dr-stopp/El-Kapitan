134ba74b85f4f01e6671ac1ce52fb79b8cbb02d6
DA-mographic <adventurequestnerd2345@gmail.com>
2026-04-02 14:56:47 -0400

    Changed filepath for ignore.txt

--- backend/src/main/java/Tokenizer/src/MultiLangTokenizer.java
+++ backend/src/main/java/Tokenizer/src/MultiLangTokenizer.java
@@ -18,7 +18,7 @@ public class MultiLangTokenizer {
     List<String> ignoredTokens = new LinkedList();
 
     public MultiLangTokenizer() throws FileNotFoundException, IOException{
-        try (BufferedReader reader = new BufferedReader(new FileReader("backend/src/Main/resources/Ignore.txt"))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("backend/src/main/resources/Ignore.txt"))) {
             String line = reader.readLine();
             while (line != null) {
                 ignoredTokens.add(line);
